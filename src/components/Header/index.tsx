753a3dca9896804bf9b103cbd350482e566b5ac2
EricoBruner <ericodasilvabruner@gmail.com>
2023-10-19 10:15:26 -0300

    att

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import logoImage from "../../assets/logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { IoIosArrowDown } from "react-icons/io";
 import { STUDENTS } from "../../data/students";
 
 export default function Header() {
+  const location = useLocation();
   const imageUrl = STUDENTS.find((s) => s.name === "Evelyn Maria")?.image;
 
   return (
@@ -16,12 +17,41 @@ export default function Header() {
         </SCBoxLogo>
 
         <nav>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/supports/me"}>Meus suportes</Link>
-          <Link to={"/student-support/me"}>Aluno suporte</Link>
-          <Link to={"/ranking"}>Top 10</Link>
-          <Link to={"/reitoria"}>Demo Universidade</Link>
-          <Link to={"/alert"}>Demo Feedback</Link>
+          <Link to={"/"} className={location.pathname === "/" ? "active" : ""}>
+            Home
+          </Link>
+          <Link
+            to={"/supports/me"}
+            className={location.pathname === "/supports/me" ? "active" : ""}
+          >
+            Meus suportes
+          </Link>
+          <Link
+            to={"/student-support/me"}
+            className={
+              location.pathname === "/student-support/me" ? "active" : ""
+            }
+          >
+            Aluno suporte
+          </Link>
+          <Link
+            to={"/ranking"}
+            className={location.pathname === "/ranking" ? "active" : ""}
+          >
+            Top 10
+          </Link>
+          <Link
+            to={"/reitoria"}
+            className={location.pathname === "/reitoria" ? "active" : ""}
+          >
+            Demo Universidade
+          </Link>
+          <Link
+            to={"/alert"}
+            className={location.pathname === "/alert" ? "active" : ""}
+          >
+            Demo Feedback
+          </Link>
         </nav>
       </div>
       <div>
@@ -62,11 +92,20 @@ const SCContainer = styled.div`
     gap: 50px;
 
     a {
+      display: flex;
+      align-items: center;
+      height: 100%;
       color: #616e84;
       font-family: Roboto;
       font-size: 16px;
       font-style: normal;
       font-weight: 700;
+      box-sizing: border-box;
+
+      &.active {
+        border-bottom: 4px solid #0078d4;
+        color: #417799;
+      }
     }
   }
 `;
